61ca871bae7dd8a859fcbdafc7e795fb2b11832c
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-10-28 18:50:16 +0200

    chore(Cmor::Contact): optimize settings registration syntax

--- cmor-contact/config/initializers/cmor-core-settings.rb
+++ cmor-contact/config/initializers/cmor-core-settings.rb
@@ -1,29 +1,35 @@
 Rails.application.config.to_prepare do
-  Cmor::Core::Settings::Setting.create!(
-    namespace: :cmor_contact,
-    key: "contact_request.sender",
-    default: nil,
-    validations: {presence: true}
-  )
+  Cmor::Core::Settings.configure do |config|
+    config.register(
+      namespace: :cmor_contact,
+      key: "contact_request.sender",
+      type: :string,
+      default: nil,
+      validations: {presence: true}
+    )
 
-  Cmor::Core::Settings::Setting.create!(
-    namespace: :cmor_contact,
-    key: "contact_request.subject.application_name",
-    default: nil,
-    validations: {presence: true}
-  )
+    config.register(
+      namespace: :cmor_contact,
+      key: "contact_request.subject.application_name",
+      type: :string,
+      default: nil,
+      validations: {presence: true}
+    )
 
-  Cmor::Core::Settings::Setting.create!(
-    namespace: :cmor_contact,
-    key: "contact_request.recipients",
-    default: nil,
-    validations: {presence: true, format: {with: /.*@.*/}}
-  )
+    config.register(
+      namespace: :cmor_contact,
+      key: "contact_request.recipients",
+      type: :array,
+      default: nil,
+      validations: {presence: true, format: {with: /.*@.*/}}
+    )
 
-  Cmor::Core::Settings::Setting.create!(
-    namespace: :cmor_contact,
-    key: "whatsapp.number",
-    default: nil,
-    validations: {presence: true, format: {with: /\A\+[\d\s]+\z/}}
-  )
+    config.register(
+      namespace: :cmor_contact,
+      key: "whatsapp.number",
+      type: :string,
+      default: nil,
+      validations: {presence: true, format: {with: /\A\+[\d\s]+\z/}}
+    )
+  end
 end
