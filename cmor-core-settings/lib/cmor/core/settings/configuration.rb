9ea4bb774be1dddf24254731742609e66d09f7e1
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-11-23 23:08:15 +0100

    feat(Cmor::Core::Settings): add the possiblity to customize the settings registration method

--- cmor-core-settings/lib/cmor/core/settings/configuration.rb
+++ cmor-core-settings/lib/cmor/core/settings/configuration.rb
@@ -29,9 +29,12 @@ module Cmor
         define_option :resource_controllers, default: -> { [] }
         define_option :service_controllers, default: -> { [] }
         define_option :sidebar_controllers, default: -> { [] }
+        define_option :register_method, default: ->(namespace:, key:, type:, default:, validations: {}) {
+          Cmor::Core::Settings::Setting.where(namespace: namespace, key: key).first_or_create!(namespace: namespace, key: key, type: type, default: default, validations: validations)
+        }
 
         def self.register(namespace:, key:, type:, default:, validations: {})
-          Cmor::Core::Settings::Setting.create!(namespace: namespace, key: key, type: type, default: default, validations: validations)
+          register_method.call(namespace: namespace, key: key, type: type, default: default, validations: validations)
         rescue StandardError => e
           puts "[Cmor::Core::Settings] Error while registering setting #{namespace}/#{key}: #{e.message}"
         end
--- cmor-core-settings/lib/generators/cmor/core/settings/install/templates/initializer.rb
+++ cmor-core-settings/lib/generators/cmor/core/settings/install/templates/initializer.rb
@@ -14,4 +14,24 @@ Cmor::Core::Settings.configure do |config|
   config.resources_controllers = -> {[
     Cmor::Core::Settings::SettingsController
   ]}
+
+  # Set the way to register settings.
+  #
+  # Default:
+  #
+  #  config.register_method = ->(namespace:, key:, type:, default:, validations: {}) {
+  #    Cmor::Core::Settings::Setting.where(namespace: namespace, key: key).first_or_create!(namespace: namespace, key: key, type: type, default: default, validations: validations)
+  #  }
+  #
+  # Example using multi tenancy:
+  #
+  # config.register_method = ->(namespace:, key:, type:, default:, validations: {}) {
+  #   Cmor::MultiTenancy::Client.all.each do |client|
+  #     Cmor::Core::Settings::Setting.where(namespace: namespace, key: key, client: client).first_or_create!(namespace: namespace, key: key, type: type, default: default, validations: validations)
+  #   end
+  # }
+  #
+  config.register_method = ->(namespace:, key:, type:, default:, validations: {}) {
+    Cmor::Core::Settings::Setting.where(namespace: namespace, key: key).first_or_create!(namespace: namespace, key: key, type: type, default: default, validations: validations)
+  }
 end
