799c7b25f7ef0e16e2ad9fa4545eb2999ecd8053
Roberto Vasquez-Angel <rva@beegoodit.de>
2025-01-05 01:59:32 +0100

    fix(Cmor::Tags): active record not being loaded lazily

--- cmor-tags/config/initializers/inject_taggable_concern.rb
+++ cmor-tags/config/initializers/inject_taggable_concern.rb
@@ -1,7 +1,9 @@
 Rails.configuration.to_prepare do
-  Cmor::Tags::Configuration.taggable_classes.each do |taggable|
-    print "[Cmor::Tags] Including Model::Cmor::Tags::TaggableConcern into #{taggable}"
-    taggable.send(:include, Model::Cmor::Tags::TaggableConcern)
-    puts " => OK"
+  ActiveSupport.on_load(:active_record) do
+    Cmor::Tags::Configuration.taggable_classes.each do |taggable|
+      print "[Cmor::Tags] Including Model::Cmor::Tags::TaggableConcern into #{taggable}"
+      taggable.send(:include, Model::Cmor::Tags::TaggableConcern)
+      puts " => OK"
+    end
   end
 end
